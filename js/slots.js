496727b8540c1fb8b1ef3d3d80683b1bd638fb21
Giovanni Tshibangu <61130107+Moodkiller20@users.noreply.github.com>
2022-11-02 03:10:32 -0400

    Delete slots.js

--- js/slots.js
+++
@@ -1,7 +0,0 @@
-
-
-
-// Initialize Cloud Firestore and get a reference to the service
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();                    // Make Fiorestore Reference.
-db.settings({timestampsInSnapshots:true}); 
